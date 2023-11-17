c2af9030e50b98083a87b03755dc9a536d46a4d6
SS-max <ss.onlogn@gmail.com>
2023-11-17 22:31:08 +0900

    あ

--- music_player_module/main_music_player_ver_0_02.py
+++ music_player_module/main_music_player_ver_0_02.py
@@ -7,8 +7,8 @@ class music_player:
         self.music_list = []
         self.music_title = 'init'
         self.proc = None
-        # self.player = vlc.MediaListPlayer()
         self.player = vlc.MediaPlayer()
+        self.pause_flag = False
         pass
 
     def start_music(self):
@@ -19,24 +19,50 @@ class music_player:
 
     def play_music(self):
         print(f'now playing... : {self.music_title}')
-        # ここに音楽を流す処理を書く。
-        
-        # command = 'cvlc test.mp3'
-        # self.proc = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE)
-
         self.player.set_mrl('test.mp3')
         self.player.play()
     
+    def pause_music(self):
+        if not self.playing_music:
+            return
+        self.playing_music = False
+        self.pause_flag = True
+        print(f'now pausing... : {self.music_title}')
+        self.player.pause()
         
+
+    def resume_music(self):
+        assert self.pause_flag, 'pause_flag is False'
+
+        if (self.playing_music) or (not self.pause_flag):
+            return
+        self.playing_music = True
+        self.pause_flag = False
+        print(f'now resuming... : {self.music_title}')
+        self.player.pause()
 
     def stop_music(self):
         if not self.playing_music: # 既に止まっているなら、何もしない
             return
         self.playing_music = False # フラグを建てて、音楽を止める。
-        # ここに音楽を止める処理を書く。
         print(f'now stopping... byebye : {self.music_title}')
-        # self.player.stop()
-        # pauseしたいときは、
-        self.player.pause()
+        self.player.stop()
 
 
+
+if __name__ == '__main__':
+    import time
+    sleep_time = 1
+    player = music_player()
+
+    player.start_music()
+    time.sleep(sleep_time)
+
+    player.pause_music()
+    time.sleep(sleep_time)
+
+    player.resume_music()
+    time.sleep(sleep_time)
+
+    player.stop_music()
+    time.sleep(sleep_time)
