89567f152e5752b9890904bd333c9413559dedbb
55_max <ss.onlogn@gmail.com>
2023-12-08 16:31:38 +0900

    a

--- chatbot/LINE_NOTIFY.py
+++ chatbot/LINE_NOTIFY.py
@@ -1,11 +1,39 @@
+import time
+import datetime
 import subprocess
 
-message = '己がこれからの日本を担う存在であることを誇りに思え'
-API_KEY_DIR = './A/chatbot/API_KEY_LINE_NOTIFY.txt'
+class notify:
+    def __init__(self):
+        self.check_in_time = ''
+        self.check_out_time = ''
 
-with open(API_KEY_DIR) as f:
-    api_key = f.read().rstrip('\n')
+    def get_check_in_time(self):
+        self.check_in_time = datetime.datetime.now().strftime('%-H時 %-M分 %-S秒')
 
-sh_cmd = f'curl https://notify-api.line.me/api/notify -X POST -H "Authorization: Bearer {api_key}" -F "message={message}"'
+    def get_check_out_time(self):
+        self.check_out_time = datetime.datetime.now().strftime('%-H時 %-M分 %-S秒')
 
-subprocess.run(sh_cmd, shell=True)  
+    def notify(self):
+        message = f'{self.check_in_time}から{self.check_out_time}まで勉強しました！お疲れ様でした！'
+        API_KEY_DIR = './chatbot/API_KEY_LINE_NOTIFY.txt'
+
+        with open(API_KEY_DIR) as f:
+            api_key = f.read().rstrip('\n')
+
+        sh_cmd = f'curl https://notify-api.line.me/api/notify -X POST -H "Authorization: Bearer {api_key}" -F "message={message}"'
+
+        subprocess.run(sh_cmd, shell=True)
+
+if __name__ == '__main__':
+    check_in_time = datetime.datetime.now().strftime('%-H時 %-M分 %-S秒')
+    time.sleep(5)
+    check_out_time = datetime.datetime.now().strftime('%-H時 %-M分 %-S秒')
+    message = f'{check_in_time}から{check_out_time}まで勉強しました！お疲れ様でした！'
+    API_KEY_DIR = './chatbot/API_KEY_LINE_NOTIFY.txt'
+
+    with open(API_KEY_DIR) as f:
+        api_key = f.read().rstrip('\n')
+
+    sh_cmd = f'curl https://notify-api.line.me/api/notify -X POST -H "Authorization: Bearer {api_key}" -F "message={message}"'
+
+    subprocess.run(sh_cmd, shell=True)  
