09c7d96b2a9ced6cc3728ae7f085415791043293
SS55-multiple-acceleration <ss.onlogn@gmail.com>
2023-12-08 11:17:00 +0900

    次は、motorとlight周りをする。それが終われば終わり。曲をＤＬするだけ～

---
+++ light_module/light.py
@@ -0,0 +1,9 @@
+class light:
+    def __init__(self):
+        pass
+
+    def on(self):
+        print('light onします')
+
+    def off(self):
+        print('light offします')
--- main_music_player_ver_0_11.py
+++ main_music_player_ver_0_11.py
@@ -32,7 +32,6 @@ def waiting_function(LIGHT_ON_FLAG):
             # motor.set_dc(3.2)
             # light.on()
             player.start_music()
-            # light on / motor きどう。
             return True
         if (not tmp_flag) and before_flag:
             LIGHT_ON_FLAG = False
@@ -40,7 +39,6 @@ def waiting_function(LIGHT_ON_FLAG):
             # light.off()
             print('light offします')
             player.pause_music()
-            # light off / motor きどう。
             continue
         if (not tmp_flag) and (not before_flag):
             continue
@@ -58,45 +56,28 @@ if __name__ == '__main__':
     print('yes')
     LIGHT_ON_FLAG = False
     
-    # light on / motor きどう。
-
     sleep_time = 0.5
 
     counter = 0
 
     player.set_session(session_path = './music_folder/')
 
-    # player.start_music()
-
     while True:
 
         waiting_function(LIGHT_ON_FLAG)
-
-        # if counter > 3:
-        #     counter = 0
-        #     Ultra_sonic.get_distance()
-        #     if not Ultra_sonic.near_flag:
-        #         print('phone off. pause music...')
-        #         player.pause_music()
-        #     else:
-        #         print('phone on.  playing music...')
-        #         player.start_music()
 
         print(counter)
         camera.detect_elements()
         if camera.face_detect:
             print('face detect')
             player.concentrate_score_down()
-            # player.start_music()
         if not camera.face_detect:
             print('not face detect')
-            # player.pause_music()
             player.concentrate_score_up()
         if camera.check_stop():
             print('camera stop')
             camera.stop_camera()
             break
-        # player.playing_music
         
         if (player.playing_music) and (not player.player.is_playing()):
             counter = 0
